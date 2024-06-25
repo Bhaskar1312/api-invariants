26db03f6c68abd334c28b00538b22a2e40f025e3
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-06-25 20:53:38 +0530

    Initial commit

---
+++ src/main/java/com/example/configascode/ConfigAsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.configascode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigAsCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigAsCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/configascode/ConfigAsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configascode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigAsCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
